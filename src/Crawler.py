23a09cc54892e295c90e4910725167c9393d2309
cjinchi <cjinchi@qq.com>
2018-07-11 12:43:44 +0800

    0711 init

--- src/Crawler.py
+++ src/Crawler.py
@@ -3,44 +3,98 @@ import urllib.error
 import re
 import urllib
 import socket
+import sqlite3
+import time
+number_pattern=re.compile('[0-9]+')
+success_question_num=0
+fail_question_num=0
 
-TAG_NAME='java'
+TAG_NAME='c++'
 
 #获取URL
-decoding_tag_name=''
+encoding_tag_name=''
 for i in TAG_NAME:                       #get decoding tag name
     if i=='+':
-        decoding_tag_name+='%%2b'        # %2b代表'+'，第一个%用于转义
+        encoding_tag_name+='%%2b'        # %2b代表'+'，第一个%用于转义
     else:
-        decoding_tag_name+=i
-URL='https://stackoverflow.com/questions/tagged/%s?page=%%d&sort=newest&pagesize=50'% decoding_tag_name
-print(URL)
+        encoding_tag_name+=i
+URL='https://stackoverflow.com/questions/tagged/%s?page=%%d&sort=newest&pagesize=50'% encoding_tag_name
+print(URL % 1)
 
 #获取总页数
 page_code=''
 try:
     request = urllib.request.Request(URL % (1))
     request.add_header("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)")
-    response = urllib.request.urlopen(request, timeout=10)
-except urllib.error.HTTPError as e:
+    response = urllib.request.urlopen(request, timeout=20)
+except urllib.error.HTTPError:
     print('【HTTP ERROR】')
-except urllib.error.URLError as e:
+    time.sleep(10)
+except urllib.error.URLError:
     print('【URL ERROR】')
-except socket.timeout as e:
+except socket.timeout:
     print('【TIMEOUT ERROR】')
-except Exception as e:
+except Exception:
     print('【Unknown ERROR】')
 else:
     page_code = response.read().decode("utf-8")
 page_num_span_pattern=re.compile('<span class="page-numbers">.*?</span>')
 page_num_span=page_num_span_pattern.findall(page_code)
-number_pattern=re.compile('[0-9]+')
 total_num_str=number_pattern.findall(page_num_span[-1])
 TOTAL_PAGE_NUM=int(total_num_str[0])
 print(TOTAL_PAGE_NUM)
 
+#TOTAL_PAGE_NUM=5       #!!! For testing  !!!
 
-
-
-
-
+#将所有的id存放到数据库中
+question_page_pattern=re.compile('"question-summary-[0-9]+"')
+question_db = sqlite3.connect('question.db')
+print('connect successfully')
+cursor=question_db.cursor()
+try:
+    cursor.execute('CREATE TABLE QUESTION (ID INT PRIMARY KEY  NOT NULL);')
+except sqlite3.OperationalError:
+    print('table exist')
+page_num=TOTAL_PAGE_NUM+1
+while page_num>1:
+    if page_num % 100 ==0:
+        time.sleep(1)
+    page_num-=1
+    url = URL % page_num
+    try:
+        request = urllib.request.Request(url)
+        request.add_header("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)")
+        response = urllib.request.urlopen(request, timeout=10)
+    except urllib.error.HTTPError:
+        print('【HTTP ERROR】')
+        time.sleep(10)
+        continue
+    except urllib.error.URLError:
+        print('【URL ERROR】')
+        print(url)
+        continue
+    except socket.timeout:
+        print('【TIMEOUT ERROR】')
+        continue
+    except Exception:
+        print('【Unknown ERROR】')
+        continue
+    else:
+        page_code = response.read().decode("utf-8")
+        raw_id_str=question_page_pattern.findall(page_code)
+        for str in raw_id_str:
+            id_str=number_pattern.findall(str)
+            id=int(id_str[0])
+            try:
+                cursor.execute("INSERT INTO QUESTION (ID)  VALUES (%d)" % id);
+            except sqlite3.IntegrityError:
+                print('exist %d' % id)
+                fail_question_num+=1
+                continue
+            else:
+                success_question_num+=1
+                print('succ num= %d' % success_question_num)
+    question_db.commit()
+question_db.close()
+print('succ = %d' % success_question_num)
+print('fail = %d' % fail_question_num)
